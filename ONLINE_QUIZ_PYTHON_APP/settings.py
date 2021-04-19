3eaa5dc61f2a6089ba9409456392dc4fa33e68b8
Sriram Sundharraman <girish.s315195@gmail.com>
2021-04-19 16:29:17 +0530

    changed something

--- ONLINE_QUIZ_PYTHON_APP/settings.py
+++ ONLINE_QUIZ_PYTHON_APP/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'ONLINE_QUIZ_PYTHON_APP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'Templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
